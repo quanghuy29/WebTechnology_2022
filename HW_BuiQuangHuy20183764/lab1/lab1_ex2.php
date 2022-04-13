8b92d130dcaee777bf439192a47a5c4f5d509fed
quanghuy29 <73057345+quanghuy29@users.noreply.github.com>
2022-04-13 22:14:18 +0700

    add lab 1

---
+++ HW_BuiQuangHuy20183764/lab1/lab1_ex2.php
@@ -0,0 +1,54 @@
+<html> 
+    <body>
+        <table border = "1">
+            <tr>
+                <th>Personal Details</th>
+                <th>
+                    <p>Imagine</p>
+                    <img src="Bui Quang Huy- CNTT.jpeg" alt="huy" style="width:300px;height:400px;">
+                </th>
+            </tr>
+            
+            <tr>
+                <th>Education process</th>
+                <th>University</th>
+            </tr>
+            
+            <tr>
+                <th>Working process</th>
+                <th>Not yet</th>
+            </tr>
+            
+            <tr>
+                <th>Society activities</th>
+                <th>Very much</th>
+            </tr>
+            
+            <tr>
+                <th>Skills</th>
+                <th>Very Good</th>
+            </tr>
+            
+            <tr>
+                <th>Experiences</th>
+                <th>So many</th>
+            </tr>
+            
+            <tr>
+                <th>Certifications</th>
+                <th>Not any</th>
+            </tr>
+            
+            <tr>
+                <th>Hobbies</th>
+                <th>Game</th>
+            </tr>
+            
+            <tr>
+                <th>Desire</th>
+                <th>Become a millionaire</th>
+            </tr>
+            
+        </table>
+    </body> 
+</html>
