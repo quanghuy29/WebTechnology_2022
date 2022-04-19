f718ed81a9624b9115decdf23b2b9cc3430c1f41
ngovanquang <banbanhbeo@outlook.com>
2022-04-19 09:19:35 +0700

    reconstruct repo

---
+++ Labs/Lab0/index.php
@@ -0,0 +1,18 @@
+/* 
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+<html>
+    <head><!-- comment -->
+        <meta http-equiv="Content-Type" content="fdf">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "hello, world! This is my first PHP project!";
+        ?>
+    </body>
+
+</html>
