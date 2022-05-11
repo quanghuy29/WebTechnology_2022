f983cfac412116ca4b02adf63ffa588c2c444118
Trần Đình Nguyên <nguyentran9122000@gmail.com>
2022-05-11 19:57:34 +0700

    test

---
+++ HW_TranDinhNguyen_20183807/Lab_2/form4radio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>receiving</title>
+    </head>
+    <body>
+        <?php
+        $email = $_POST['email'];
+        $contact = $_POST['contact'];
+        print("your email is $email");
+        print("contact is $contact");
+        ?>
+    </body>
+</html>
