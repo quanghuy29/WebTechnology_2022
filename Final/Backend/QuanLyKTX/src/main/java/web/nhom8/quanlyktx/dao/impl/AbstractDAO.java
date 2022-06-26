00ca729a220bbac04f3d0ad87d58c6ebf9ab508a
ngovanquang <banbanhbeo@outlook.com>
2022-06-26 17:03:22 +0700

    NgoVanQuang_20183813: xay dung ham chung query()

--- Final/Backend/QuanLyKTX/src/main/java/web/nhom8/quanlyktx/dao/impl/AbstractDAO.java
+++ Final/Backend/QuanLyKTX/src/main/java/web/nhom8/quanlyktx/dao/impl/AbstractDAO.java
@@ -3,9 +3,8 @@ package web.nhom8.quanlyktx.dao.impl;
 import web.nhom8.quanlyktx.dao.GenericDAO;
 import web.nhom8.quanlyktx.mapper.RowMapper;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -25,9 +24,64 @@ public class AbstractDAO<T> implements GenericDAO<T> {
         }
     }
 
+    private void setParameter(PreparedStatement statement, Object... parameters)
+    {
+        try {
+            for(int i = 0; i < parameters.length; i++)
+            {
+                Object parameter = parameters[i];
+                int index = i + 1;
+                if(parameter instanceof Long) {
+                    statement.setLong(index, (Long) parameter);
+                } else if (parameter instanceof Integer) {
+                    statement.setInt(index, (Integer) parameter);
+                } else if (parameter instanceof Timestamp) {
+                    statement.setTimestamp(index, (Timestamp) parameter);
+                } else if (parameter instanceof String) {
+                    statement.setString(index, (String) parameter);
+                }
+            }
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     @Override
-    public <T1> List<T1> query(String sql, RowMapper<T1> rowMapper, Object... parameters) {
-        return null;
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        List<T>             results     = new ArrayList<>();
+        Connection          connection  = null;
+        PreparedStatement   statement   = null;
+        ResultSet           resultSet   = null;
+
+        try {
+            connection  = getConnection();
+            statement   = connection.prepareStatement(sql);
+            setParameter(statement, parameters);
+            resultSet   = statement.executeQuery();
+
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet));
+            }
+
+            return results;
+        } catch (SQLException e) {
+            return null;
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException e) {
+                return null;
+            }
+        }
     }
 
     @Override
