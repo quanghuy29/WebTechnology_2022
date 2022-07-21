155462fa5fa4722d72c5b9df72af67b1d6bdbb57
Maj64 <manhnamhungdj1@gmail.com>
2022-07-21 11:16:55 +0700

    add route

--- Final/Fontend/src/App.js
+++ Final/Fontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Layout from "./component/Layout";
 import NotFound from "./pages/NotFound/NotFound";
-import RequireAuth from './protectAuth/RequireAuth/RequireAuth';
+import RequireAuth from "./protectAuth/RequireAuth/RequireAuth";
 import Login from "./pages/login";
 import Room from "./pages/RoomManager/room";
 import Students from "./pages/Students/Students";
@@ -26,8 +26,9 @@ class App extends Component {
             <Route path="login" element={<SignIn />} />
 
             <Route element={<RequireAuth allowedRoles={ROLES.status} />}>
-          <Route path="/" element={<AccountManager />} />
-        </Route>
+              <Route path="/account-manager" element={<AccountManager />} />
+              <Route path="/room" element={<Room />} />
+            </Route>
 
             <Route path="*" element={<NotFound />} />
           </Route>
