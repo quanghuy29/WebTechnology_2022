60ff0232ea4b1d1a106dca522f6ad97fb3a09cd1
quanghuy29 <73057345+quanghuy29@users.noreply.github.com>
2022-07-23 20:57:58 +0700

    Update JWTFilter.java

--- Final/Backend/QuanLyKTX/src/main/java/web/nhom8/quanlyktx/filter/JWTFilter.java
+++ Final/Backend/QuanLyKTX/src/main/java/web/nhom8/quanlyktx/filter/JWTFilter.java
@@ -28,18 +28,12 @@ public class JWTFilter implements Filter {
         String url = request.getRequestURI();
         System.out.println(url);
         String rolecode = "NAN";
-        String username;
-        String msg;
-        int status;
+
         if (!url.startsWith("/QuanLyKTX_war_exploded/api-login"))
         {
             String method = request.getHeader("Access-Control-Request-Headers");
             if (Objects.equals(method, "authorization") || Objects.equals(method, "authorization,content-type") ){
-                response.addHeader("Access-Control-Allow-Origin", "*");
-                response.addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
-                response.addHeader("Access-Control-Allow-Credentials","true");
-                response.addHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
-
+                setHeaderResponse(response);
                 response.setStatus(HttpServletResponse.SC_ACCEPTED);
                 return;
             }
@@ -50,11 +44,7 @@ public class JWTFilter implements Filter {
                 JWTParseObject jwtParseObject = TokenJWTUtils.getUserIdAndRoleCodeFromJWT(jwtToken);
                 System.out.println(jwtParseObject.getUserId());
                 System.out.println(jwtParseObject.getRoleCode());
-                if (!rolecode.isEmpty())
-                {
-                    rolecode = jwtParseObject.getRoleCode();
-                }
-                username = jwtParseObject.getUserId();
+                rolecode = jwtParseObject.getRoleCode();
             }
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
@@ -65,59 +55,22 @@ public class JWTFilter implements Filter {
             if (rolecode.equalsIgnoreCase("QLSV") || rolecode.equalsIgnoreCase("QLT"))
             {
                 filterChain.doFilter(servletRequest, servletResponse);
-            } else {
-                response.addHeader("Access-Control-Allow-Origin", "*");
-                response.addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
-                response.addHeader("Access-Control-Allow-Credentials","true");
-                response.addHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-                msg = "Unauthorize!";
-                status = 300;
-                ResponseObject responseObject = new ResponseObject();
-                responseObject.setMessage(msg);
-                responseObject.setStatus(status);
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.writeValue(response.getOutputStream(), responseObject);
-            }
+            } else sendMessageError(response);
         } else if (url.startsWith("/QuanLyKTX_war_exploded/*")) {
             if (rolecode.equalsIgnoreCase("QLT"))
             {
                 filterChain.doFilter(servletRequest, servletResponse);
-            } else {
-                msg = "Unauthorize!";
-                status = 300;
-                ResponseObject responseObject = new ResponseObject();
-                responseObject.setMessage(msg);
-                responseObject.setStatus(status);
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.writeValue(response.getOutputStream(), responseObject);
-            }
+            } else sendMessageError(response);
         } else if (url.startsWith("/QuanLyKTX_war_exploded/room") || url.startsWith("/QuanLyKTX_war_exploded/room/student")) {
             if (rolecode.equalsIgnoreCase("QLPO"))
             {
                 filterChain.doFilter(servletRequest, servletResponse);
-            } else {
-                msg = "Unauthorize!";
-                status = 300;
-                ResponseObject responseObject = new ResponseObject();
-                responseObject.setMessage(msg);
-                responseObject.setStatus(status);
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.writeValue(response.getOutputStream(), responseObject);
-            }
+            } else sendMessageError(response);
         } else if (url.startsWith("/QuanLyKTX_war_exploded/")) {
             if (rolecode.equalsIgnoreCase("KT"))
             {
                 filterChain.doFilter(servletRequest, servletResponse);
-            } else {
-                msg = "Unauthorize!";
-                status = 300;
-                ResponseObject responseObject = new ResponseObject();
-                responseObject.setMessage(msg);
-                responseObject.setStatus(status);
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.writeValue(response.getOutputStream(), responseObject);
-            }
+            } else sendMessageError(response);
         }
 
     }
@@ -125,5 +78,23 @@ public class JWTFilter implements Filter {
     @Override
     public void destroy() {
         Filter.super.destroy();
+    }
+
+    public void setHeaderResponse(HttpServletResponse response){
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
+        response.addHeader("Access-Control-Allow-Credentials","true");
+        response.addHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+    }
+
+    private void sendMessageError(HttpServletResponse response) throws IOException{
+        setHeaderResponse(response);
+        String msg = "Unauthorize!";
+        int status = 300;
+        ResponseObject responseObject = new ResponseObject();
+        responseObject.setMessage(msg);
+        responseObject.setStatus(status);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), responseObject);
     }
 }
